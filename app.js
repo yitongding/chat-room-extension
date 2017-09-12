470a103ed2fc760da1a433ecc4f07e16a38180de
Yitong Ding <dyt111@gmail.com>
2017-09-11 21:42:51 -0400

    init project

---
+++ app.js
@@ -0,0 +1,28 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+
+// const userRoutes = require('./private/router/userRoutes');
+
+const isDeveloping = process.env.NODE_ENV !== 'prod';
+const port = isDeveloping ? 8080 : process.env.PORT;
+
+const mongodbUrl = isDeveloping ? 'mongodb://localhost/react-app' : process.env.MONGODB_URL;
+mongoose.Promise = global.Promise;
+mongoose.connect(mongodbUrl);
+
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// app.use('/api', userRoutes);
+app.use(express.static(`${__dirname}/public`));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.listen(port);
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+// const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  context: path.join(__dirname, 'src'),
+  entry: ['babel-polyfill', './js/index.js'],
+  devtool: 'inline-source-map',
+  module: {
+    loaders: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: [
+            'react',
+            'env',
+            'stage-3',
+          ],
+        },
+      },
+    ],
+  },
+  output: {
+    path: `${__dirname}/public`,
+    filename: 'index.min.js',
+  },
+};
